50e44430a94ab27456c2547e3f83c1ca687b87b0
Stantrh <stanislas.troha8@etu.univ-lorraine.fr>
2024-05-24 11:34:30 +0200

    neuille humide

---
+++ Seance6bis/Service/ServiceAgenda.java
@@ -0,0 +1,9 @@
+import java.rmi.RemoteException;
+import java.rmi.Remote;
+
+public interface ServiceAgenda extends Remote{
+
+    public Contact accederContact(String pseudo) throws RemoteException;
+
+    public void ajouterContact(Contact c) throws RemoteException;
+}
