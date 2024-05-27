c410e6e3d3e8dd24aefa22a18fee42789885f486
Stantrh <stanislas.troha8@etu.univ-lorraine.fr>
2024-05-27 13:07:32 +0200

    ajout de la seance 7

---
+++ Seance7/Dessin.java
@@ -0,0 +1,10 @@
+import java.awt.Color;
+
+public class Dessin { 
+    public int x, y;
+    public Color c;
+
+    public String toString(){
+        return "Dessin : ("+this.x+", "+this.y+") couleur = "+this.c ; 
+    }   
+}
